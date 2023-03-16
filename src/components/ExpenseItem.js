cf7f114dadb01ce4e3104121b811b90005675f08
RyanGormican <ryan.gormican@gmail.com>
2023-03-16 01:55:39 -0600

    a
    a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Icon } from '@iconify/react';
 const ExpenseItem = (props) => {
+	const { dispatch } = useContext(AppContext);
+
+	const handleDeleteExpense = () => {
+		dispatch({
+			type: 'DELETE_EXPENSE',
+			payload: props.id,
+		});
+	};
 	return (
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
 			{props.name}

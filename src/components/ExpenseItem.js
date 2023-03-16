cd34f378296f12f745f1a0e118160e003c0b63ad
RyanGormican <ryan.gormican@gmail.com>
2023-03-16 00:51:45 -0600

    a
    a

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+const ExpenseItem = (props) => {
+	return (
+		<li className='list-group-item d-flex justify-content-between align-items-center'>
+			{props.name}
+			<div>
+				<span className='badge badge-primary badge-pill mr-3'>
+					${props.cost}
+					<Icon icon="mdi:delete-circle" />
+				</span>
+			</div>
+		</li>
+	);
+};
+export default ExpenseItem;

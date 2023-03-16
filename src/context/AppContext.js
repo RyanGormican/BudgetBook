2cb59d9a9fa2dd75b01b6e5b91f7c502427efcdc
RyanGormican <ryan.gormican@gmail.com>
2023-03-16 03:59:43 -0600

    a
    a

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,18 +3,21 @@ import {createContext, useReducer} from 'react';
 const AppReducer =(state, action) => {
 	switch(action.type){
 		case 'ADD_EXPENSE':
+			const expense = [...state.expenses, action.payload];
+			localStorage.setItem('BudgetBook-expenses', JSON.stringify(expenses));
 			return {
 				...state,
-				expenses:[...state.expenses, action.payload],
+				expenses,
 			};
 		case 'DELETE_EXPENSE':
+			const updatedExpenses = state.expenses.filter((expense)=> expense.id !== action.payload);
+			localStorage.setItem('BudgetBook-expenses', JSON.stringify(updatedExpenses));
 			return {
 				...state,
-				expenses: state.expenses.filter(
-				(expense)=> expense.id !== action.payload
-				),
+				updatedExpenses
 			};
 		case 'SET_BUDGET':
+			localStorage.setItem('BudgetBook-budget', action.payload);
 			return {
 				...state,
 				budget: action.payload,
@@ -22,6 +25,14 @@ const AppReducer =(state, action) => {
 		default:
 			return state;
 	}
+};
+const getStorage = () => {
+	const budget = localStorage.getItem('BudgetBook-budget') || initialState.budget;
+	const exepnses = JSON.parse(localStorage.getItem('BudgetBook-expenses') || initialState.expenses;
+	return {
+		budget:parseInt(budget),
+		expenses,
+	};
 };
 const initialState= {
 	budget: 1000,
@@ -33,7 +44,12 @@ const initialState= {
 export const AppContext = createContext();
 
 export const AppProvider = (props) => {
-	const[state,dispatch] = useReducer(AppReducer, initialState);
+	const[state,dispatch] = useReducer(AppReducer, initialState, getStorage);
+
+	useEffect(() =>{
+		localStorage.setItem('budget',state.budget);
+		localStorage.setItem('expenses',JSON.stringify(state.expenses));
+	},[state]);
 
 	return(<AppContext.Provider value={{
 		budget: state.budget,

f03a88c8602f091a7662530158023e7abd65f3b6
RyanGormican <ryan.gormican@gmail.com>
2023-03-16 01:47:08 -0600

    a
    a

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,14 +1,14 @@
 import React,{useContext} from 'react';
 import { AppContext} from '../context/AppContext';
 const Remaining = () => {
-	const { expense, budget } =useContext(AppContext);
+	const { expenses, budget } =useContext(AppContext);
 	const totalExpenses = expenses.reduce((total,item)=>{
 		return (total = total + item.cost)
 	},0);
 	return (
 		<div className = 'alert alert-success'>
 			<span>
-			Remaining: ${buadget - totalExpenses}
+			Remaining: ${budget - totalExpenses}
 			</span>
 		</div>
 	);

036d0387dbe2f3e2ac20a9995930a1c74aed036c
RyanGormican <ryan.gormican@gmail.com>
2023-03-17 01:11:37 -0600

    a
    a

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -3,7 +3,7 @@ import { AppContext} from '../context/AppContext';
 const Remaining = () => {
 	const { expenses, budget } =useContext(AppContext);
 	const totalExpenses = expenses.reduce((total,item)=>{
-		return (total = total + item.cost) * 1.00;
+		return (total = total + item.cost*1.00) ;
 	},0);
 
 	const alertType = totalExpenses > budget ? 'alert alert-danger' :'alert alert-success';

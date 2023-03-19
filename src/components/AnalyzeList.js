f2ca24c302cd321ecce8ad08fb31f682972a88de
RyanGormican <ryan.gormican@gmail.com>
2023-03-19 00:37:27 -0600

    a
    a

--- src/components/AnalyzeList.js
+++ src/components/AnalyzeList.js
@@ -1,43 +1,48 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import{Chart, ArcElement, Tooltip, Legend} from 'chart.js';
-import {Pie} from 'react-chartjs-2';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 Chart.register(ArcElement, Tooltip, Legend);
 
-
- export const costData ={
-  labels: [ ],
-  datasets:[
-  {
-  label: 'Cost by Tag',
-  data: [],
-  backgroundColor: [
-
-
-  ],
-  },
-  ],
-  };
-
 const AnalyzeList = () => {
-  const { expenses } = useContext(AppContext);
-  
- 
+  const { expenses, budget } = useContext(AppContext);
 
   const tagCosts = {};
-  expenses.forEach(expense => {
+  expenses.forEach((expense) => {
     const { tag, cost } = expense;
     if (tag in tagCosts) {
-      tagCosts[tag] += cost;
+      tagCosts[tag] += (cost * 1.00 );
     } else {
       tagCosts[tag] = cost;
     }
   });
 
-  
+  const tags = Object.keys(tagCosts);
+  const unusedBudget = budget - Object.values(tagCosts).reduce((acc, cost) => acc + cost, 0);
+
+  const labels = [...tags, 'Unused'];
+  const data = [...Object.values(tagCosts), unusedBudget];
+  const backgroundColor = labels.map(() => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgba(${r}, ${g}, ${b}, 0.6)`;
+  });
+
+  const costData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Cost',
+        data: data,
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
+
   return (
-    <div>
-    <Pie data = {costData} />
+    <div style={{ width:'50vw', height:'40vh'}}>
+      <Pie data={costData} />
     </div>
   );
 };

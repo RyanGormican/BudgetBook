e9d4bfd151a0dd7b715f56b43766f0a1d8822361
RyanGormican <ryan.gormican@gmail.com>
2023-03-19 00:32:39 -0600

    a
    a

--- src/components/AnalyzeList.js
+++ src/components/AnalyzeList.js
@@ -1,11 +1,45 @@
-import React, {useContext, useState } from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import{Chart, ArcElement, Tooltip, Legend} from 'chart.js';
+import {Pie} from 'react-chartjs-2';
+Chart.register(ArcElement, Tooltip, Legend);
+
+
+ export const costData ={
+  labels: [ ],
+  datasets:[
+  {
+  label: 'Cost by Tag',
+  data: [],
+  backgroundColor: [
+
+
+  ],
+  },
+  ],
+  };
 
 const AnalyzeList = () => {
-	
-	return (
-		<div>
-		Test
-		</div>
-	);
+  const { expenses } = useContext(AppContext);
+  
+ 
+
+  const tagCosts = {};
+  expenses.forEach(expense => {
+    const { tag, cost } = expense;
+    if (tag in tagCosts) {
+      tagCosts[tag] += cost;
+    } else {
+      tagCosts[tag] = cost;
+    }
+  });
+
+  
+  return (
+    <div>
+    <Pie data = {costData} />
+    </div>
+  );
 };
+
 export default AnalyzeList;

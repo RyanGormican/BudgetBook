ff5ab8adce162e068d266b57069bab942116754c
RyanGormican <ryan.gormican@gmail.com>
2023-03-19 01:15:34 -0600

    a
    a

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,11 +3,20 @@ import { AppContext } from '../context/AppContext';
 
 
 const Settings = () => {
-	const { state, dispatch } = useContext (AppContext);
-	const [decimalPrecision, setDecimalPrecision] = useState(state.settings.decimalPrecision);
+	const { settings, dispatch } = useContext (AppContext);
+	const [decimalPrecision, setDecimalPrecision] = useState(settings.decimalPrecision);
+	const handleFormSubmit= (e)=> {
+		e.preventDefault();
+		dispatch({
+			type: 'UPDATE_SETTINGS',
+			paylod: settings
+		});
+	};
 	return (
+		<form onSubmit = {handleFormSubmit}>
 		<div>
 		</div>
+		</form>
 	);
 };
 export default Settings;

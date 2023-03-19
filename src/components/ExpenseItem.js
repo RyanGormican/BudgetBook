72b9138c99e6fe852ae21b9adf83d8858fb4d75a
RyanGormican <ryan.gormican@gmail.com>
2023-03-19 02:05:32 -0600

    a
    a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,26 +4,26 @@ import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
 	const { dispatch } = useContext(AppContext);
 
-	const date = props.time ? new Date(props.time)
+	const date = props.time ? new Date(props.time) : new Date();
+	const dateTimeString = date.toLocaleString()
+
+
 	const handleDeleteExpense = () => {
 		dispatch({
 			type: 'DELETE_EXPENSE',
 			payload: props.id,
 		});
 	};
+	const theTime = props.time ? (
+	<span className='btn btn-primary'>
+				{theTime}
+	</span>
+	) : null;
 	return (
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
 			{props.name}
 			<div>
-			{props.time? (
-				<span className='btn btn-primary'>
-				{props.time.toLocalDateString()}
-				</span>
-			) : (
-				<span className='btn btn-primary'>
-				{new Date().toLocaleDateString()}
-				</span>
-			)}
+				{theTime}
 				<span className='btn btn-primary'>
 				{props.tag}
 				</span>

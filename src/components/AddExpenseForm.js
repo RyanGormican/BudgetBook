ad2920b1ce820ea9390f7169e1947f0977c93380
RyanGormican <ryan.gormican@gmail.com>
2023-03-25 17:59:21 -0600

    a
    a

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -2,12 +2,26 @@ import React, {useContext, useState} from 'react';
 import {AppContext} from '../context/AppContext';
 import {v4 as uuidv4} from 'uuid';
 import {GrabButtonColors, GrabTextColors} from './Utility';
+import { Icon } from '@iconify/react';
 const AddExpenseForm = () => {
 const {dispatch, style, settings} = useContext(AppContext);
 const [name,setName] = useState('');
 const [cost,setCost] = useState('');
-const [tag, setTag] = useState('');
+const [tags, setTags] = useState(['']);
 const buttonStyle = { backgroundColor: GrabButtonColors(), color: GrabTextColors()};
+const handleTagChange = (event, index)=>{
+		const newTags = [...tags];
+		newTags[index] = event.target.value;
+		setTags(newTags);
+	};
+	const handleAddTag = () => {
+		setTags([...tags,'']);
+	};
+	const handleRemoveTag = (index)=>{
+		const newTags = [...tags];
+		newTags.splice(index,1);
+		setTags(newTags);
+	};
 const onSubmit = (event) =>{
 	event.preventDefault();
 	const now = new Date();
@@ -19,7 +33,7 @@ const onSubmit = (event) =>{
 		id:uuidv4(),
 		name:name,
 		cost: parseFloat(cost).toFixed(2),
-		tag:tag,
+		tags:tags,
 		time:localDate.toISOString().slice(0,16),
 		timestamp:new Date().getTime(),
 		lastUpdated:new Date().getTime(),
@@ -29,7 +43,7 @@ const onSubmit = (event) =>{
 		payload: expense,
 
 	});
-};
+ };
 	return (
 		<form onSubmit={onSubmit}>
 			<div className='row'>
@@ -58,17 +72,31 @@ const onSubmit = (event) =>{
 					/>
 				</div> 
 				<div className='col-sm'>
-					<button className='btn' style={buttonStyle}> Tag </button>
+					<button className='btn' style={buttonStyle}> Tags </button>
+				{tags.map((tag,index) => (
+				<div key={index}>
 					<input 
 						required='required' 
 						type='text'
 						className='form-control'
 						id='name'
 						value={tag}
-						onChange={(event)=> setTag(event.target.value)}
+						onChange={(event)=> handleTagChange(event, index)}
 					/>
+					{index === tags.length - 1 && (
+						<button className='btn' style={buttonStyle} onClick = {handleAddTag}> 
+							<Icon icon="material-symbols:add" /> 
+						</button>
+					)}
+					{tags.length > 1 && (
+						<button className='btn' style={buttonStyle} onClick={()=> handleRemoveTag(index)}> 
+							<Icon icon="material-symbols:minus" /> 
+						</button>
+					)}
+					</div> 
+				))}
 				</div> 
-				</div> 
+			</div>
 				<div class='row mt-3'>
 				<div className='col-sm'>
 					<button type='submit' className='btn' style={buttonStyle}>

a99f4d29d195d3ec8baa3f8815ddc7e14f867e31
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2025-02-21 00:26:51 -0600

    Update ViewButtons.js

--- src/components/ViewButtons.js
+++ src/components/ViewButtons.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GrabButtonColors, GrabTextColors } from './Utility';
 import { Icon } from '@iconify/react';
+
 const ViewButtons = ({ setView }) => {
     return (
         <div className='view-buttons mt-3 text-center'>
@@ -21,30 +22,9 @@ const ViewButtons = ({ setView }) => {
             <button
                 className="btn"
                 style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
-                onClick={() => setView('Calendar')}
-            >
-                Calendar <Icon icon="mdi:calendar" />
-            </button>
-            <button
-                className="btn"
-                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
                 onClick={() => setView('Budget')}
             >
                 Budget <Icon icon="majesticons:money-line" />
-            </button>
-            <button
-                className="btn"
-                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
-                onClick={() => setView('Export')}
-            >
-                Export <Icon icon="mdi:export"  />
-            </button>
-            <button
-                className="btn"
-                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
-                onClick={() => setView('Settings')}
-            >
-                Settings <Icon icon="material-symbols:settings" />
             </button>
             <button
                 className="btn"
@@ -56,13 +36,33 @@ const ViewButtons = ({ setView }) => {
             <button
                 className="btn"
                 style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
+                onClick={() => setView('Calendar')}
+            >
+                Calendar <Icon icon="mdi:calendar" />
+            </button>
+            <button
+                className="btn"
+                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
                 onClick={() => setView('Customize')}
             >
                 Customize <Icon icon="mdi:art" />
             </button>
+            <button
+                className="btn"
+                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
+                onClick={() => setView('Settings')}
+            >
+                Settings <Icon icon="material-symbols:settings" />
+            </button>
+            <button
+                className="btn"
+                style={{ backgroundColor: GrabButtonColors(), color: GrabTextColors() }}
+                onClick={() => setView('Export')}
+            >
+                Export <Icon icon="mdi:export"  />
+            </button>
         </div>
     );
 };
-
 
 export default ViewButtons;
